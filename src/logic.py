1308dc670c216d44f94d5bee6d40e4b830e145a7
Bailey Bakerson <bailey.bakerson@gmail.com>
2023-02-24 21:36:41 -0500

    edit

--- src/logic.py
+++ src/logic.py
@@ -10,11 +10,13 @@ Date: 2023-02-24
 
 def update_display(display, event):
     
+    # performs operations if 0-9
     if event.isdigit():
         if display == '0':
             return event
         return display + event
 
+    # performs ops for +-*/=.
     match event:
         case "+":
             if display[-1].isdigit():
@@ -29,11 +31,6 @@ def update_display(display, event):
             if display[-1].isdigit():
                 display += '/'
 
-        case "+/-":
-            if display[-1].isdigit():
-                display += '+'
-                print("HIUIUH")
-
         case ".":
             if display[-1].isdigit() and '.' not in display.replace('*',' ')\
                 .replace('/',' ').replace('+',' ').replace('-',' ').split()[-1]:
@@ -44,12 +41,5 @@ def update_display(display, event):
                 display = str(eval(display))
             except ZeroDivisionError:
                 display = "ERROR"
-
-    # convert to scientific notation
-    # if len(display) > 10:
-    #     if '.' in display:
-    #         display = "{:e}".format(float(display))
-    #     else:
-    #         display = "{:e}".format(int(display))
 
     return display
